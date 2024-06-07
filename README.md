cd344f885ec513511fbc9de14f64fa4a755b4022
geekymon2 <geekymon2@gmail.com>
2024-06-07 21:13:23 +1000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # terraform-azure
+
+Demonstrate how terraform can be used with azure cloud provider.
+
+### Login to Azure
+
+```
+az account login
+```
+
+### Show logged in account
+
+```
+az account show
+```
---
+++ main.tf
@@ -0,0 +1,18 @@
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = "00000000-0000-0000-0000-000000000000"
+  tenant_id       = "1a8baaf8-8855-40d4-9585-ae6328c54068"
+}
